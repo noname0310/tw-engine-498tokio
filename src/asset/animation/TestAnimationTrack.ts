51c63c598a5e8dba017be95c1a2993e1c09b70cc
noname <48761044+noname0310@users.noreply.github.com>
2022-04-08 23:41:53 +0900

    Update TestAnimationTrack.ts

--- src/asset/animation/TestAnimationTrack.ts
+++ src/asset/animation/TestAnimationTrack.ts
@@ -3,15 +3,15 @@ import { AnimationKey, InterpolationKind } from "../script/animation/AnimationKe
 import { AnimationTrack } from "../script/animation/AnimationTrack";
 
 export const testAnimationTrack1 = (() => {
-    const zeroVector3 = new Vector3(0, 0, 0);
+    const zero = new Vector3(0, 0, 0);
     let acc = 0;
     return AnimationTrack.createVector3Track([
-        AnimationKey.createRefType(acc, new Vector3(0, 0, 0), InterpolationKind.Cubic, zeroVector3, zeroVector3),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, 2, 0), InterpolationKind.Cubic, zeroVector3, zeroVector3),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2, 0), InterpolationKind.Cubic, zeroVector3, zeroVector3),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, 2, 0), InterpolationKind.Cubic, zeroVector3, zeroVector3),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, -2, 0), InterpolationKind.Cubic, zeroVector3, zeroVector3),
-        AnimationKey.createRefType(acc += 100, new Vector3(1, 1, 0), InterpolationKind.Cubic, zeroVector3, zeroVector3),
+        AnimationKey.createRefType(acc += 0.0, new Vector3(0, 0, 0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3(2, 2, 0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2, 0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2, 2, 0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3(2, -2, 0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3(1, 1, 0), InterpolationKind.Cubic, zero, zero),
         AnimationKey.createRefType(acc += 100, new Vector3(0, 0, 0), InterpolationKind.Linear),
         AnimationKey.createRefType(acc += 100, new Vector3(2, 2, 0), InterpolationKind.Linear),
         AnimationKey.createRefType(acc += 100, new Vector3(-2, -2, 0), InterpolationKind.Linear),
@@ -28,15 +28,15 @@ export const testAnimationTrack1 = (() => {
 })();
 
 export const testAnimationTrack2 = (() => {
-    const zeroVector2 = new Vector2(0, 0);
+    const zero = new Vector2(0, 0);
     let acc = 0;
     return AnimationTrack.createVector2Track([
-        AnimationKey.createRefType(acc, new Vector2(0, 0), InterpolationKind.Cubic, zeroVector2, zeroVector2),
-        AnimationKey.createRefType(acc += 100, new Vector2(2, 2), InterpolationKind.Cubic, zeroVector2, zeroVector2),
-        AnimationKey.createRefType(acc += 100, new Vector2(-2, -2), InterpolationKind.Cubic, zeroVector2, zeroVector2),
-        AnimationKey.createRefType(acc += 100, new Vector2(-2, 2), InterpolationKind.Cubic, zeroVector2, zeroVector2),
-        AnimationKey.createRefType(acc += 100, new Vector2(2, -2), InterpolationKind.Cubic, zeroVector2, zeroVector2),
-        AnimationKey.createRefType(acc += 100, new Vector2(1, 1), InterpolationKind.Cubic, zeroVector2, zeroVector2),
+        AnimationKey.createRefType(acc += 0.0, new Vector2(0, 0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector2(2, 2), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector2(-2, -2), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector2(-2, 2), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector2(2, -2), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector2(1, 1), InterpolationKind.Cubic, zero, zero),
         AnimationKey.createRefType(acc += 100, new Vector2(0, 0), InterpolationKind.Linear),
         AnimationKey.createRefType(acc += 100, new Vector2(2, 2), InterpolationKind.Linear),
         AnimationKey.createRefType(acc += 100, new Vector2(-2, -2), InterpolationKind.Linear),
