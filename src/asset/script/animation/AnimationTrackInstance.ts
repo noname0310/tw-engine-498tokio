04f9d24387fa9f9585d46d6112d7220d2bbde0bd
noname <48761044+noname0310@users.noreply.github.com>
2022-04-09 00:45:23 +0900

    Update AnimationTrackInstance.ts

--- src/asset/script/animation/AnimationTrackInstance.ts
+++ src/asset/script/animation/AnimationTrackInstance.ts
@@ -32,6 +32,8 @@ export class AnimationTrackInstance<T> {
 
         let startKeyIndex = this._currentFrameIndex;
 
+        if (frameTime < 0) frameTime = 0;
+        
         while (0 < startKeyIndex && frameTime < keys[startKeyIndex].frame) startKeyIndex -= 1;
         while (startKeyIndex < keys.length - 1 && frameTime >= keys[startKeyIndex + 1].frame) startKeyIndex += 1;
 
