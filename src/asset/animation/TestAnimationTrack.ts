031daf6025da4710a0471add687b98109ae16916
noname <48761044+noname0310@users.noreply.github.com>
2022-04-08 23:47:04 +0900

    Update TestAnimationTrack.ts

--- src/asset/animation/TestAnimationTrack.ts
+++ src/asset/animation/TestAnimationTrack.ts
@@ -6,24 +6,24 @@ export const testAnimationTrack1 = (() => {
     const zero = new Vector3(0, 0, 0);
     let acc = 0;
     return AnimationTrack.createVector3Track([
-        AnimationKey.createRefType(acc += 0.0, new Vector3(0, 0, 0), InterpolationKind.Cubic, zero, zero),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, 2, 0), InterpolationKind.Cubic, zero, zero),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2, 0), InterpolationKind.Cubic, zero, zero),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, 2, 0), InterpolationKind.Cubic, zero, zero),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, -2, 0), InterpolationKind.Cubic, zero, zero),
-        AnimationKey.createRefType(acc += 100, new Vector3(1, 1, 0), InterpolationKind.Cubic, zero, zero),
-        AnimationKey.createRefType(acc += 100, new Vector3(0, 0, 0), InterpolationKind.Linear),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, 2, 0), InterpolationKind.Linear),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2, 0), InterpolationKind.Linear),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, 2, 0), InterpolationKind.Linear),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, -2, 0), InterpolationKind.Linear),
-        AnimationKey.createRefType(acc += 100, new Vector3(1, 1, 0), InterpolationKind.Linear),
-        AnimationKey.createRefType(acc += 100, new Vector3(0, 0, 0), InterpolationKind.Step),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, 2, 0), InterpolationKind.Step),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2, 0), InterpolationKind.Step),
-        AnimationKey.createRefType(acc += 100, new Vector3(-2, 2, 0), InterpolationKind.Step),
-        AnimationKey.createRefType(acc += 100, new Vector3(2, -2, 0), InterpolationKind.Step),
-        AnimationKey.createRefType(acc += 100, new Vector3(1, 1, 0), InterpolationKind.Step)
+        AnimationKey.createRefType(acc += 0.0, new Vector3( 0,  0,  0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3( 2,  2,  0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2,  0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2,  2,  0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3( 2, -2,  0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3( 1,  1,  0), InterpolationKind.Cubic, zero, zero),
+        AnimationKey.createRefType(acc += 100, new Vector3( 0,  0,  0), InterpolationKind.Linear),
+        AnimationKey.createRefType(acc += 100, new Vector3( 2,  2,  0), InterpolationKind.Linear),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2,  0), InterpolationKind.Linear),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2,  2,  0), InterpolationKind.Linear),
+        AnimationKey.createRefType(acc += 100, new Vector3( 2, -2,  0), InterpolationKind.Linear),
+        AnimationKey.createRefType(acc += 100, new Vector3( 1,  1,  0), InterpolationKind.Linear),
+        AnimationKey.createRefType(acc += 100, new Vector3( 0,  0,  0), InterpolationKind.Step),
+        AnimationKey.createRefType(acc += 100, new Vector3( 2,  2,  0), InterpolationKind.Step),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2, -2,  0), InterpolationKind.Step),
+        AnimationKey.createRefType(acc += 100, new Vector3(-2,  2,  0), InterpolationKind.Step),
+        AnimationKey.createRefType(acc += 100, new Vector3( 2, -2,  0), InterpolationKind.Step),
+        AnimationKey.createRefType(acc += 100, new Vector3( 1,  1,  0), InterpolationKind.Step)
     ]);
 })();
 
