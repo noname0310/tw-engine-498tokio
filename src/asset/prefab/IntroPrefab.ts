779858e9a75595ba81349bc5d448c7f3237b6368
noname <48761044+noname0310@users.noreply.github.com>
2022-04-18 00:01:19 +0900

    Update IntroPrefab.ts

--- src/asset/prefab/IntroPrefab.ts
+++ src/asset/prefab/IntroPrefab.ts
@@ -12,6 +12,7 @@ import {
 } from "the-world-engine";
 import { Vector3, Vector2 } from "three/src/Three";
 import { GlowSpriteAtlasRenderer } from "../script/render/GlowSpriteAtlasRenderer";
+import { HorizontalObjectsAnimator } from "../script/render/HorizontalObjectsAnimator";
 import ImageFirework1 from "../image/firework1.png";
 import ImageFirework2 from "../image/firework2.png";
 import ImageFirework3 from "../image/firework3.png";
@@ -20,7 +21,6 @@ import ImageMoon from "../image/moon.png";
 import ImageMoonEmission from "../image/moon_emission.png";
 import ImageBackground from "../image/intro_background.jpg";
 import ImageGrass from "../image/grass.png";
-import { HorizontalObjectsAnimator } from "../script/render/HorizontalObjectsAnimator";
 
 export type IntroObjects = {
     moonGroup: PrefabRef<GameObject>;
@@ -31,7 +31,11 @@ export type IntroObjects = {
     blackScreen: PrefabRef<CssHtmlElementRenderer>;
     moon: PrefabRef<CssSpriteRenderer>;
     grass1: PrefabRef<GameObject>;
-    grassRender: PrefabRef<HorizontalObjectsAnimator>;
+    grassRender1: PrefabRef<HorizontalObjectsAnimator>;
+    grass2: PrefabRef<GameObject>;
+    grassRender2: PrefabRef<HorizontalObjectsAnimator>;
+    grass3: PrefabRef<GameObject>;
+    grassRender3: PrefabRef<HorizontalObjectsAnimator>;
 };
 
 export class IntroPrefab extends Prefab {
@@ -43,7 +47,11 @@ export class IntroPrefab extends Prefab {
     private _blackScreen = new PrefabRef<CssHtmlElementRenderer>();
     private _moon = new PrefabRef<CssSpriteRenderer>();
     private _grass1 = new PrefabRef<GameObject>();
-    private _grassRender = new PrefabRef<HorizontalObjectsAnimator>();
+    private _grassRender1 = new PrefabRef<HorizontalObjectsAnimator>();
+    private _grass2 = new PrefabRef<GameObject>();
+    private _grassRender2 = new PrefabRef<HorizontalObjectsAnimator>();
+    private _grass3 = new PrefabRef<GameObject>();
+    private _grassRender3 = new PrefabRef<HorizontalObjectsAnimator>();
 
     public getObjects(introObjects: PrefabRef<IntroObjects>): this {
         introObjects.ref = {
@@ -55,7 +63,11 @@ export class IntroPrefab extends Prefab {
             blackScreen: this._blackScreen,
             moon: this._moon,
             grass1: this._grass1,
-            grassRender: this._grassRender
+            grassRender1: this._grassRender1,
+            grass2: this._grass2,
+            grassRender2: this._grassRender2,
+            grass3: this._grass3,
+            grassRender3: this._grassRender3
         };
 
         return this;
@@ -63,7 +75,7 @@ export class IntroPrefab extends Prefab {
 
     public override make(): GameObjectBuilder {
         return this.gameObjectBuilder
-            .withChild(this.instantiater.buildGameObject("moon_group", new Vector3(0, 0, 2))
+            .withChild(this.instantiater.buildGameObject("moon_group", new Vector3(0, 0, -7))
                 .getGameObject(this._moonGroup)
 
                 .withChild(this.instantiater.buildGameObject("firework1", new Vector3(-3, 3, 0))
@@ -106,7 +118,7 @@ export class IntroPrefab extends Prefab {
                         c.filter.dropShadow = new CssDropShadow(-0.3, -0.3, 1, new Color(1, 1, 1));
                     })))
 
-            .withChild(this.instantiater.buildGameObject("black_screen", new Vector3(0, 0, 1))
+            .withChild(this.instantiater.buildGameObject("black_screen", new Vector3(0, 0, -8))
                 .withComponent(CssHtmlElementRenderer, c => {
                     const div = document.createElement("div");
                     div.style.backgroundColor = "#000000";
@@ -116,7 +128,7 @@ export class IntroPrefab extends Prefab {
                 })
                 .getComponent(CssHtmlElementRenderer, this._blackScreen))
 
-            .withChild(this.instantiater.buildGameObject("background", new Vector3(0, 0, -1), undefined, new Vector3().setScalar(1.55))
+            .withChild(this.instantiater.buildGameObject("background", new Vector3(0, 0, -10), undefined, new Vector3().setScalar(1.55))
                 .withComponent(CssTilemapRenderer, c => {
                     c.tileResolutionX = 4;
                     c.tileResolutionY = 36;
@@ -140,7 +152,7 @@ export class IntroPrefab extends Prefab {
                     };
                 }))
 
-            .withChild(this.instantiater.buildGameObject("grass1", new Vector3(-30, -13, 3), undefined, new Vector3().setScalar(6))
+            .withChild(this.instantiater.buildGameObject("grass1", new Vector3(-30, -17, -5), undefined, new Vector3().setScalar(6))
                 .withComponent(HorizontalObjectsAnimator, c => {
                     c.prefab = class extends Prefab {
                         public make(): GameObjectBuilder {
@@ -154,10 +166,50 @@ export class IntroPrefab extends Prefab {
                     };
 
                     c.spawnCount = 7;
-                    c.padding = 1.59;
+                    c.padding = 1.57;
                 })
-                .getComponent(HorizontalObjectsAnimator, this._grassRender)
+                .getComponent(HorizontalObjectsAnimator, this._grassRender1)
                 .getGameObject(this._grass1))
+                
+            .withChild(this.instantiater.buildGameObject("grass2", new Vector3(-30, -14, -6), undefined, new Vector3().setScalar(4))
+                .withComponent(HorizontalObjectsAnimator, c => {
+                    c.prefab = class extends Prefab {
+                        public make(): GameObjectBuilder {
+                            return this.gameObjectBuilder
+                                .withComponent(CssSpriteRenderer, c => {
+                                    c.asyncSetImagePath(ImageGrass);
+                                    c.centerOffset = new Vector2(0, 0.5);
+                                    c.filter.blur = 0.1;
+                                    c.filter.brightness = 0.7;
+                                });
+                        }
+                    };
+
+                    c.spawnCount = 11;
+                    c.padding = 1.57;
+                })
+                .getComponent(HorizontalObjectsAnimator, this._grassRender2)
+                .getGameObject(this._grass2))
+
+            .withChild(this.instantiater.buildGameObject("grass3", new Vector3(-30, -12, -7), undefined, new Vector3().setScalar(3))
+                .withComponent(HorizontalObjectsAnimator, c => {
+                    c.prefab = class extends Prefab {
+                        public make(): GameObjectBuilder {
+                            return this.gameObjectBuilder
+                                .withComponent(CssSpriteRenderer, c => {
+                                    c.asyncSetImagePath(ImageGrass);
+                                    c.centerOffset = new Vector2(0, 0.5);
+                                    c.filter.blur = 0.05;
+                                    c.filter.brightness = 0.6;
+                                });
+                        }
+                    };
+
+                    c.spawnCount = 14;
+                    c.padding = 1.57;
+                })
+                .getComponent(HorizontalObjectsAnimator, this._grassRender3)
+                .getGameObject(this._grass3))
         ;
     }
 }
